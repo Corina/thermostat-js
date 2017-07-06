fec9fb1c1ac52c00424a4e4366f510297ec2ee98
Corina <fluturelcorina@gmail.com>
2017-07-06 23:31:33 +0100

    Adding color scheme to energy usage

--- src/thermostatjQuery.js
+++ src/thermostatjQuery.js
@@ -1,29 +1,43 @@
+
 $(document).ready(function(){
+  
   var thermostat = new Thermostat;
+  
   $('#current-temp').text(thermostat.temperature);
   $('#max-temp').text(thermostat.maxTempPSMOn);
   $('#min-temp').text(thermostat.minTemp);
   $('#readPSM').text(thermostat.readPSM);
   $('#usage').text(thermostat.usage);
+  
   $("#Up").on("click", function() {
     thermostat.up();
-    thermostat.currentEnergyUsage();
-    $('#current-temp').text(thermostat.temperature);
-    $('#usage').text(thermostat.usage);
+    updateText();
   });
+  
+  
   $("#Down").on("click", function() {
     thermostat.down();
-    thermostat.currentEnergyUsage();
-    $('#current-temp').text(thermostat.temperature);
-    $('#usage').text(thermostat.usage);
+    updateText();
   });
+  
   $("#Reset").on("click", function() {
     thermostat.reset();
     $('#current-temp').text(thermostat.temperature);
   });
-
+  
   $("#PSM").on("click", function() {
     thermostat.powerSavingModeSwitch();
     $('#readPSM').text(thermostat.readPSM);
   });
+  
+  function updateText() {
+    thermostat.currentEnergyUsage();
+    $('#current-temp').text(thermostat.temperature);
+    $('#usage').text(thermostat.usage);
+    $('#usage').attr("class", thermostat.usage);
+  };
+  
+  updateText();
+  
+  
 });
