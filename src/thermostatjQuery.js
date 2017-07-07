605acdc0387e2ce7567a581f9813f17510efa615
Jasper King-Harman <j.kingharman@googlemail.com>
2017-07-07 15:39:02 +0100

    Creating functionality to allow user input for weather

--- src/thermostatjQuery.js
+++ src/thermostatjQuery.js
@@ -1,44 +1,44 @@
 
 $(document).ready(function(){
-  
+
   var thermostat = new Thermostat;
-  
+
   $('#current-temp').text(thermostat.temperature);
   $('#max-temp').text(thermostat.maxTempPSMOn);
   $('#min-temp').text(thermostat.minTemp);
   $('#readPSM').text(thermostat.readPSM);
   $('#usage').text(thermostat.usage);
-  
+
   $("#Up").on("click", function() {
     thermostat.up();
     updateText();
   });
-  
+
   $("#Down").on("click", function() {
     thermostat.down();
     updateText();
   });
-  
+
   $("#Reset").on("click", function() {
     thermostat.reset();
     $('#current-temp').text(thermostat.temperature);
   });
-  
+
   $("#PSM").on("click", function() {
     thermostat.powerSavingModeSwitch();
     $('#readPSM').text(thermostat.readPSM);
   });
-  
+
   function updateText() {
     thermostat.currentEnergyUsage();
     $('#current-temp').text(thermostat.temperature);
     $('#usage').text(thermostat.usage);
     $('#usage').attr("class", thermostat.usage);
   };
-  
+
   updateText();
-  
-  
+
+
   $.ajax({
     url: "http://api.aerisapi.com/observations/london,uk?client_id=A7wgFltFhcp5SBW05g5Mi&client_secret=DvqwzsXvzOrWgfkaiw0YvXhhZPB4JfsTERkBhR8S",
     dataType: "jsonp",
@@ -52,22 +52,39 @@ $(document).ready(function(){
       }
     }
   });
-  
+
   function CreateUrl(city) {
-    return "http://api.aerisapi.com/observations/" + city + "?client_id=A7wgFltFhcp5SBW05g5Mi&client_secret=DvqwzsXvzOrWgfkaiw0YvXhhZPB4JfsTERkBhR8S"
+    return "http://api.aerisapi.com/observations/" + city.replace('city=','') + ",uk?client_id=A7wgFltFhcp5SBW05g5Mi&client_secret=DvqwzsXvzOrWgfkaiw0YvXhhZPB4JfsTERkBhR8S"
   };
-  
-  
-  $("#city").submit(function(city) {
-    url = CreateUrl(city);
+
+
+  $("#city").submit(function(event) {
+    event.preventDefault();
+    value = CreateUrl($( this ).serialize())
+    console.log(value);
+    $.ajax({
+      url: CreateUrl($( this ).serialize()),
+      dataType: "jsonp",
+      success: function(json) {
+        if (json.success == true) {
+          weather_ob = json.response.ob
+          location_ob = json.response.place
+          $('#london-weather').html('The current weather in ' + location_ob.name + ' ' + weather_ob.weather.toLowerCase() + ' with a temperature of ' + weather_ob.tempC + '°');
+        }
+        else {
+          alert('An error occurred: ' + json.error.description);
+        }
+      }
+    });
+
   });
-  
+
   // Access ID
   // A7wgFltFhcp5SBW05g5Mi
   //
   // Secret Key
   // DvqwzsXvzOrWgfkaiw0YvXhhZPB4JfsTERkBhR8S
-  
-  
-  
+
+
+
 });
